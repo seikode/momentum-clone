7c0168c5f910f9a84bae9ed15624a496e0fd1ee5
seikode <lsylsy0403@gmail.com>
2025-01-23 15:48:59 +0900

    start clone!

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-alert("hi");
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {}
+
+loginButton.addEventListener("click", onLoginBtnClick);
