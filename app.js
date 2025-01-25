28439aae9cfc4dc0512d226e7c25e60645d5e338
seikode <lsylsy0403@gmail.com>
2025-01-25 15:54:58 +0900

    update

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-function onLoginBtnClick() {}
+const HIDDEN_CLASSNAME = "hidden";
 
-loginButton.addEventListener("click", onLoginBtnClick);
+function onLoginSubmit(event) {
+  event.preventDefault();
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
