931e5a67880beb5625a0f8cca78ef089f571d458
seikode <lsylsy0403@gmail.com>
2025-02-03 18:23:58 +0900

    update

--- js/weather.js
+++ js/weather.js
@@ -6,12 +6,12 @@ const API_KEY = "a64f387c5366febc3b00c76244fdae38";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lng = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       city.innerText = data.name;
-      weather.innerText = `${data.weather[0].main}${data.main.temp}`;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
     });
 }
 
